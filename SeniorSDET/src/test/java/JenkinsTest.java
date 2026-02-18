6b0130fb6836f9ee357399c503b247c2c6a5a5f0
SiteshSDET <seniorsdetsitesh@gmail.com>
2026-02-18 21:55:14 +0530

    Initial Commits
    Added Test files for testing jenkins

---
+++ SeniorSDET/src/test/java/JenkinsTest.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class JenkinsTest {
+
+    @Test
+    public void jenkinsConfigureTest(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.get("https://www.google.com/");
+        String browserTitle = driver.getTitle();
+        System.out.println("Browser title is : " + browserTitle);
+    }
+}
